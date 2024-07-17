2847af0e5a0f09f007d1c1581a598bef7a1777ed
venchatla <87869870+venchatla@users.noreply.github.com>
2024-07-17 12:46:19 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-west-2"
+}
 
+variable "cluster_name" {
+  description = "EKS cluster name"
+  default     = "example-cluster"
+}
